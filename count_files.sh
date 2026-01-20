c57f3408403c38458337a552962586ee62d73ac7
Nazar Morziev <morzevnazar2005@gmail.com>
2026-01-20 10:45:13 +0000

    Add bash script to count files in /etc

---
+++ count_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TARGET_DIR="/etc"
+FILE_COUNT=0
+
+for item in "$TARGET_DIR"/*; do
+    if [ -f "$item" ] && [ ! -L "$item" ]; then
+        FILE_COUNT=$((FILE_COUNT + 1))
+    fi
+done
+
+echo "Number of regular files in $TARGET_DIR: $FILE_COUNT"
+
